8ba9cbdd6412e949c36fbf975c9563fcc44a2c0d
Joshua Fitzgerald <joshua.fitzgerald@slickdeals.net>
2024-06-19 09:26:48 -0700

    feat: added for switch for ver. 1 | ver.2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from 'react-dom'
 import './index.scss'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import App from './App';
+import AppV2 from './AppV2';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function Main() {
+  return (
+    <React.StrictMode>
+      <Router>
+        <nav className="versionNav">
+          <ul>
+            <li>
+              <Link to="/">ver. 1</Link>
+            </li>
+            <li>|</li>
+            <li>
+              <Link to="/v2">ver. 2</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/v2" element={<AppV2 />} />
+        </Routes>
+      </Router>
+    </React.StrictMode>
+  )
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
